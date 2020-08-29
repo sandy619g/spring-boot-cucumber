7898a06cd01e2886d6f4b7c81666780bad344ede
Sandesh <sandy619g@gmail.com>
2020-08-29 12:42:07 +0530

    initial commit

---
+++ src/main/java/com/tdd/CucumberDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucumberDemoApplication.class, args);
+	}
+
+}
